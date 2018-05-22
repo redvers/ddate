20a71a6a8b9b45cec367db71e9e091217d919f77
Redvers Davies <red@infect.me>
2018-05-22 15:30:51 +0000

    Using the new fancy mix format

--- lib/ddate.ex
+++ lib/ddate.ex
@@ -4,14 +4,13 @@ defmodule Ddate do
   which converts Gregorian Dates to Discordian Dates.
   """
 
-
   @doc """
   Ddate.ddate
 
   Returns Discordian Date as a string for the current date.
   """
   def ddate do
-    {date, _time} = :erlang.localtime
+    {date, _time} = :erlang.localtime()
     ddate(date)
   end
 
@@ -23,38 +22,47 @@ defmodule Ddate do
   """
   def ddate({year, month, day}) do
     isTibs = :calendar.is_leap_year(year)
-    getdate(isTibs, daynum{year, month, day})
+
+    getdate(isTibs, daynum({year, month, day}))
     |> formatddate(year)
   end
+
   defp getdate(false, daynum) do
-    {false, div(daynum, 73), rem(daynum, 73)+1, rem(daynum, 5)}
+    {false, div(daynum, 73), rem(daynum, 73) + 1, rem(daynum, 5)}
   end
+
   defp getdate(true, daynum) when daynum < 59 do
-    {false, div(daynum, 73), rem(daynum, 73)+1, rem(daynum, 5)}
+    {false, div(daynum, 73), rem(daynum, 73) + 1, rem(daynum, 5)}
   end
+
   defp getdate(true, 59) do
     {:sttibs, 0, 0, 0}
   end
+
   defp getdate(true, daynum) when daynum > 59 do
     newdaynum = daynum - 1
-    {false, div(newdaynum, 73), rem(newdaynum, 73)+1, rem(newdaynum, 5)}
+    {false, div(newdaynum, 73), rem(newdaynum, 73) + 1, rem(newdaynum, 5)}
   end
+
   defp formatddate({:sttibs, _, _, _}, year) do
     "St Tibs Day, YOLD #{year + 1166}"
   end
+
   defp formatddate({false, season, 5, day}, year) do
     "#{longday(day)}, St #{longpatron(season)} of #{longseason(season)} YOLD #{year + 1166}"
   end
+
   defp formatddate({false, season, 50, day}, year) do
     "#{longday(day)}, #{longholyday(season)} of #{longseason(season)} YOLD #{year + 1166}"
   end
+
   defp formatddate({false, season, date, day}, year) do
     "#{longday(day)}, #{date}#{ordinal(date)} of #{longseason(season)} YOLD #{year + 1166}"
   end
 
   defp daynum({year, month, day}) do
-    :calendar.date_to_gregorian_days({year,month,day})
-    |> :erlang.-(:calendar.date_to_gregorian_days({year,1,1}))
+    :calendar.date_to_gregorian_days({year, month, day})
+    |> :erlang.-(:calendar.date_to_gregorian_days({year, 1, 1}))
   end
 
   defp longseason(0), do: "Chaos"
@@ -81,10 +89,9 @@ defmodule Ddate do
   defp longholyday(3), do: "Bureflux"
   defp longholyday(4), do: "Afflux"
 
-  defp ordinal(number) when number in [10,11,12,13], do: "th"
+  defp ordinal(number) when number in [10, 11, 12, 13], do: "th"
   defp ordinal(number) when rem(number, 10) == 1, do: "st"
   defp ordinal(number) when rem(number, 10) == 2, do: "nd"
   defp ordinal(number) when rem(number, 10) == 3, do: "rd"
   defp ordinal(_), do: "th"
-
 end
--- mix.exs
+++ mix.exs
@@ -2,15 +2,17 @@ defmodule Ddate.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ddate,
-     version: "0.0.2",
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: description(),
-     package: package(),
-     name: "Ddate",
-     deps: deps()]
+    [
+      app: :ddate,
+      version: "0.0.2",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      name: "Ddate",
+      deps: deps()
+    ]
   end
 
   def application, do: []
